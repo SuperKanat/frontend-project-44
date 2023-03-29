57b829fa5920fefebd141efe7741b6ac9eaee58a
kanat <vasilyev.yuriy@hexly.ru>
2023-03-29 18:05:40 +0300

    calc

---
+++ src/games/calc.js
@@ -0,0 +1,29 @@
+import createGame from '../index.js';
+import getRandomNumber from '../getRandom.js';
+
+const rule = 'What is the result of the expression?';
+
+const operationSigns = ['+', '-', '*'];
+const calculate = (randomNumber1, randomNumber2, operation) => {
+  switch (operation) {
+    case '+':
+      return randomNumber1 + randomNumber2;
+    case '-':
+      return randomNumber1 - randomNumber2;
+    case '*':
+      return randomNumber1 * randomNumber2;
+    default:
+      throw new Error(`operation ${operation} is not supported`);
+  }
+};
+
+const calculateExpression = () => {
+  const randomNumber1 = getRandomNumber(1, 50);
+  const randomNumber2 = getRandomNumber(1, 50);
+  const randomOperationSign = operationSigns[getRandomNumber(0, operationSigns.length - 1)];
+  const correctAnswer = calculate(randomNumber1, randomNumber2, randomOperationSign);
+  const question = `${randomNumber1} ${randomOperationSign} ${randomNumber2}`;
+  return [question, String(correctAnswer)];
+};
+
+export default () => createGame(calculateExpression, rule);
